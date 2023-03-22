a2c48774f12a07d52b54dcfe5e450898b950b958
Quentin <109299545+ClueXIII@users.noreply.github.com>
2023-03-22 13:53:17 +0100

    exo 3 finit

--- exercices/exo3.php
+++ exercices/exo3.php
@@ -1,31 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>3</title>
+</head>
+<body>
+exo 3
+<br>
 <?php
+    echo "<h1>Tableaux simples</h1>";
     // Declaration 1
-    $tab_a[0] = "G";
-    $tab_a[1] = "A";
-    $tab_a[2] = "R";
-    $tab_a[3] = "N";
-    $tab_a[4] = "I";
-    $tab_a[5] = "E";
-    $tab_a[6] = "R";
+    $NOM[0] = "G";
+    $NOM[1] = "A";
+    $NOM[2] = "R";
+    $NOM[3] = "N";
+    $NOM[4] = "I";
+    $NOM[5] = "E";
+    $NOM[6] = "R";
     echo "<p>Tableau A = ";
-    print_r($tab_a);
+    print_r($NOM);
     echo "</p>";
-	
-    // Declaration 2 
-    $tab_b = array("Q", "U", "E", "N", "T","I","N");
-    echo "<p>Tableau B = ";
-    print_r($tab_b);
-    echo "</p>";
-	
-    // Declaration 3
-    $tab_c[] = "G";
-    $tab_c[] = "R";
-    $tab_c[] = "A";
-    $tab_c[] = "N";
-    $tab_c[] = "I";
-    $tab_c[] = "E";
-    $tab_c[] = "R";
-    echo "<P>Tableau C = ";
-    print_r($tab_c);
 
+    // Declaration 2
+    $PRENOM = array("Q", "U", "E", "N", "T","I","N");
+    echo "<p>Tableau B = ";
+    print_r($PRENOM);
+    echo "</p>";
+
+    // Declaration 3
+    $SURNOM[] = "G";
+    $SURNOM[] = "R";
+    $SURNOM[] = "A";
+    $SURNOM[] = "N";
+    $SURNOM[] = "I";
+    $SURNOM[] = "E";
+    $SURNOM[] = "R";
+    echo "<P>Tableau C = ";
+    print_r($SURNOM);
+
+    echo "<h1>Tableaux associatifs</h1>";
+    $sites = array (
+        "github" => "https://github.com/ClueXIII",
+        "youtube" => "https://www.youtube.com",
+        "discord" => "https://discord.com",
+        "ENT" => "https://ent.univ-avignon.fr" );
+    echo "Tableau de sites = ";
+    print_r($sites);
+    echo '<br><a style="color:green"> La case github du tableau de sites contient ';
+    echo "le site ", $sites["github"], "</a>";
+
+    echo "<h1>Parcours des tableaux simples</h1>";
+    echo "<p>" . implode("-", $NOM) . "_" . implode("-", $PRENOM) . "_" . implode("-", $SURNOM) . "</p>";
+
+    echo "<h1>Parcours des tableaux associatifs<h1>";
+    $sports = array(
+        "Football" => "https://www.fifa.com/",
+        "Basketball" => "https://www.nba.com/",
+        "Escalade" => "https://www.ffme.fr/",
+        "Natation" => "https://www.fina.org/",
+        "Tennis" => "https://www.atptour.com/",
+    );
+    echo "<h3>Version 1 d'affichage</h3>";
+    while (list($sport, $url) = each($sports)) {
+        echo "$sport (<a HREF=\"$url\">$url</a>)<br/>\n";
+    }
+    echo "<h3>Version 2 d'affichage</h3>";
+    foreach ($sports as $sport => $url) {
+        echo "$sport (<a HREF=\"$url\">$url</a>)<br/>\n";
+    }
 ?>
+</body>
+</html>
